ab9f001b37e027235c6b0859cb8a5d131ed5d096
Berk Tepebag <berktepebag@gmail.com>
2020-02-05 09:00:07 +0300

    Update ad2.py Add ad3.py & ad_teach.launch
    1. ad2.py is the raw image to steering converter. Takes in a model.h5 file.
    
    2. ad3.py publishes steering angles and forward/reverse commands to rc_car/teleop_cmd
    
    3. ad_teach.launch file is used for saving images and teleop commands from joystick.

--- scripts/ad3.py
+++ scripts/ad3.py
@@ -17,6 +17,9 @@ import rospy
 # Ros Messages
 from sensor_msgs.msg import CompressedImage
 
+#Teleop Message
+from self_driving_rc_car.msg import RcCarTeleop
+
 VERBOSE=False
 
 global graph,model,sess
@@ -25,12 +28,15 @@ graph = tf.get_default_graph()
 #No module named tkinter -> https://askubuntu.com/questions/815874/importerror-no-named-tkinter-please-install-the-python3-tk-package
 model = load_model('model.h5')
 
+teleop_msg = RcCarTeleop()
+teleop_pub = rospy.Publisher("rc_car/teleop_cmd", RcCarTeleop, queue_size=1)
+
 class image_feature:
 
     def __init__(self):
         
         # subscribed Topic
-        self.subscriber = rospy.Subscriber("/sync/resized_image/compressed",
+        self.subscriber = rospy.Subscriber("/image_color/compressed",
             CompressedImage, self.callback,  queue_size = 1)
         if VERBOSE :
             print ("subscribed to /sync/resized_image/compressed")
@@ -47,25 +53,37 @@ class image_feature:
         # image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
 
-        scale_percent = 50 # percent of original size
+        scale_percent = 25 # percent of original size
         width = int(image_np.shape[1] * scale_percent/100)
         height = int(image_np.shape[0] * scale_percent/100)
         dim = (width, height)
+        # print("dim w: %f h: %f ",width,height)
 
         resized = cv2.resize(image_np,dim,interpolation = cv2.INTER_AREA)
         cropped = resized[20:180,]      
 
         with graph.as_default():
             steering_angle = float(model.predict(cropped[None, :, :, :], batch_size=1))
+            teleop_msg.servo = steering_angle * 1.1
+        
+        teleop_msg.forward = 0.25
+        teleop_msg.reverse = 1.0
 
-        print("steering_angle: ",steering_angle)
+        teleop_pub.publish(teleop_msg)
+
+        # print("steering_angle: ",steering_angle)
+
+
 
 def main():	
-	
-	ic = image_feature()
-	rospy.init_node('img_to_steering', anonymous=True)
 
-	rospy.spin()
+    
+    # rospy.init_node("autonomous_drive", anonymous=True)
+    
+    ic = image_feature()
+    rospy.init_node('img_to_steering', anonymous=True)
+    
+    rospy.spin()
 
 
 if __name__ == '__main__':
